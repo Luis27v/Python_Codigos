5686eb2dc3d57c9a3fcbf89a0fe754dfacb0f85f
Luis27v <luiseduardo27v@gmail>
2026-05-18 15:31:20 -0300

    1094

--- 1094.py
+++ 1094.py
@@ -3,3 +3,10 @@ coelhos = ratos = sapos = total = 0
 for i in range (int(input())):
     qtde, animal = input(). split()
     total += int(qtde)
+
+    if animal == 'c':
+        coelhos += int(qtde)
+    elif animal == 'r':
+        ratos += int(qtde)
+    elif animal == 's':
+        sapos += int(qtde)
