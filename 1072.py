a82753018f47efa7a360c2242a2419f05b82f199
Luis27v <luiseduardo27v@gmail>
2026-05-18 15:06:11 -0300

    1072 v1

---
+++ 1072.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+in_ = 0 
+out = 0
+
+for i in range(n):
+    x = int(input())
+
+    if 10 <= x <= 20:
+        int_ += 1
+    else:
+        out == 1
+
+print (f'{in_} in')
+print (f'{out} out')
