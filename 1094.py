6e2684478f8a7fe6fb94b7628613d2e915925970
Luis27v <luiseduardo27v@gmail>
2026-05-18 15:35:50 -0300

    1094

--- 1094.py
+++ 1094.py
@@ -10,3 +10,11 @@ for i in range (int(input())):
         ratos += int(qtde)
     elif animal == 's':
         sapos += int(qtde)
+
+print(f'Total: {total} cobaias')
+print(f'Total de Coelhos: {coelhos}')
+print(f'Total de Ratos: {ratos}')
+print(f'Total de Sapos: {sapos}')
+print (f'Percentual de coelhos: {((100.0/total) * coelhos): .2f}')
+print (f'Percentual de ratis: {((100.0/total) * ratos): .2f}')
+print (f'Percentual de sapis: {((100.0/total) * sapos): .2f}')
